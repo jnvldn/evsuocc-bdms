2ff5ee6a674189e6ae39ccbbb676ca8a5aa8967a
Jury Catingub <jurycatingub@gmail.com>
2026-05-10 20:44:40 +0800

    update reports
    10
    relocate php file

--- reports.php
+++ reports.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '/require_admin.php';
+require_once __DIR__ . '/require_login.php';
 require_once __DIR__ . '/db.php';
 require_once __DIR__ . '/reports_helpers.php';
 
